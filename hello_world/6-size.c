7a5ea28143837c609dec97d71aa8ccd92b51581c
mpesaran <mahsa.pesaran@gmail.com>
2024-10-21 19:01:28 +1100

    Edit trailing whitespace

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -8,7 +8,7 @@
  *@lli: The long long integer data
  *@f: The float data
  *
- *Description: What returns from sizeof function is 
+ *Description: What returns from sizeof function is
  *type size_t and zu should use for it
  *Return: zero if successfuly executed
 */
@@ -19,10 +19,11 @@ int main(void)
 	long int li;
 	long long int lli;
 	float f;
+
 	printf("Size of a char: %zu byte(s)\n", sizeof(c));
 	printf("Size of an int: %zu byte(s)\n", sizeof(i));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(li));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(lli));
 	printf("Size of a float: %zu byte(s)\n", sizeof(f));
 	return (0);
-}	
+}
