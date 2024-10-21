b90362884206fbe65632179c511156fd2b4699cc
mpesaran <mahsa.pesaran@gmail.com>
2024-10-21 18:31:47 +1100

    Print size of different types

---
+++ hello_world/6-size.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+/**
+ * main - This function is to print sizes of data types
+ * @c: The character data
+ * @i: The integer data
+ * @li: The long integer data
+ * @lli: The long long integer data
+ * @f: The float data
+ *
+ * Return: zero if successfuly executed
+*/
+int main(void)
+{
+	char c;
+	int i;
+ 	long int li;
+	long long int lli;
+	float f;
+
+	printf("Size of a char: %zu byte(s)\n" , sizeof(c));
+	printf("Size of an int: %zu byte(s)\n" , sizeof(i));
+	printf("Size of a long int: %zu byte(s)\n" , sizeof(li));
+	printf("Size of a long long int: %zu byte(s)\n" , sizeof(lli));
+	printf("Size of a long long int: %zu byte(s)\n" , sizeof(f));
+
+	return (0);
+}	
