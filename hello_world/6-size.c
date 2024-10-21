ead709e4e9ab994bdc8b6099063d2047887df332
mpesaran <mahsa.pesaran@gmail.com>
2024-10-21 18:42:40 +1100

    Correct the output string and comments

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -8,6 +8,7 @@
  * @lli: The long long integer data
  * @f: The float data
  *
+ * Description: What returns from sizeof function is type size_t and zu should use for it
  * Return: zero if successfuly executed
 */
 int main(void)
@@ -22,7 +23,7 @@ int main(void)
 	printf("Size of an int: %zu byte(s)\n" , sizeof(i));
 	printf("Size of a long int: %zu byte(s)\n" , sizeof(li));
 	printf("Size of a long long int: %zu byte(s)\n" , sizeof(lli));
-	printf("Size of a long long int: %zu byte(s)\n" , sizeof(f));
+	printf("Size of a float: %zu byte(s)\n" , sizeof(f));
 
 	return (0);
 }	
