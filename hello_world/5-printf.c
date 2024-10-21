b76121bc75c154146a57b8f114e883d90f060baa
mpesaran <mahsa.pesaran@gmail.com>
2024-10-21 17:58:53 +1100

    Output a string using printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+/**
+ * main - This function will print a string using printf
+ *
+ * Return : non negative number if executed successfully
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
