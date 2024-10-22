b7a619da89c1f57899b4020a7aba588a82c480fe
mpesaran <mahsa.pesaran@gmail.com>
2024-10-22 18:08:58 +1100

    Print numbers in ascending order

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers in ascending order
+ *
+ * Return: numbers in ascending order seperated with space and comma
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+		if ( i != 57)
+		{
+			putchar(' ');
+			putchar(',');
+		}
+		else
+		{
+			putchar(36);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
