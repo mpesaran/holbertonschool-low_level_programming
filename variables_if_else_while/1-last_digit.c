f800ac847b6eea4225b45a38462673c70b02e42f
mpesaran <mahsa.pesaran@gmail.com>
2024-10-22 16:01:53 +1100

    Edit documenting stylrs

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -6,27 +6,26 @@
 * main - This is about comparing last digit of a random number
 *
 * Return: last digit is more than 5 or zero orless than 5
-*  
 */
 int main(void)
 {
 	int n;
-	int last_digit;
+	int lst_dgt;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	last_digit = n % 10;
-	if (last_digit > 5)
+	lst_dgt = n % 10;
+	if (lst_dgt > 5)
 	{
-		printf("Last digit of %i is %i and is greater than 5\n", n, last_digit);
+		printf("Last digit of %i is %i and is greater than 5\n", n, lst_dgt);
 	}
-	else if (last_digit == 0)
+	else if (lst_dgt == 0)
 	{
-		printf("Last digit of %i is %i and is 0\n", n, last_digit);
+		printf("Last digit of %i is %i and is 0\n", n, lst_dgt);
 	}
 	else
 	{
-		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, last_digit);
-	}	
+		printf("Last digit of %i is %i and is less than 6 and not 0\n",n, lst_dgt);
+	}
 	return (0);
 }
