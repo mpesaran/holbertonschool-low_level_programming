3348bdb0dd9b3af10afb78ca34d1ff2063768164
mpesaran <mahsa.pesaran@gmail.com>
2024-10-24 08:39:48 +1100

    Add a header file and includeit in c file

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,10 +1,20 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - print a string
+ *
+ * Return: 0 for success
 */
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
 	return (0);
 }
