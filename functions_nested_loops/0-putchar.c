f61efdb0480066578d0214be625dfc6a8e6d05f9
mpesaran <mahsa.pesaran@gmail.com>
2024-10-24 08:12:18 +1100

    Print a string

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/**
+ * main - print a string
+*/
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
---
+++ functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This is third project of this week. We will practice functions and nested loops in C!
