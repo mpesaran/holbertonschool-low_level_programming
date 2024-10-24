6e028c77eac2efc46a644cf8f95ca37b7e69effe
mpesaran <mahsa.pesaran@gmail.com>
2024-10-24 15:09:08 +1100

    Add a condition for when the number is 98

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -9,14 +9,18 @@
 */
 void print_to_98(int n)
 {
-	if (n < 98)
+	if (n == 98)
+	{
+		printf("98");
+	}
+	else if (n < 98)
 	{
 		while (n <= 98)
 		{
 			if ( n == 98)
-			{	
+			{
 				printf("%d", n);
-			}		
+			}
 			printf("%d, \n", n);
 			n++;
 		}
