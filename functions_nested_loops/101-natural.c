359845c50e159dd6a8065452a5717178962abcd1
mpesaran <mahsa.pesaran@gmail.com>
2024-10-24 17:15:15 +1100

    Add a function to add multiples of 3 or 5

---
+++ functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * sum - will add all the numbers less than 1024 that are multiple of 3 or 5
+ */
+void sum(void)
+{
+	int result = 0;
+	int n = 1;
+
+	while (n < 1024)
+	{
+		if (n % 3 == 0 || n % 5 ==0)
+		{
+			result = result + n;
+		}
+		n++
+	}
+	printf("%d\n", result);
+}
