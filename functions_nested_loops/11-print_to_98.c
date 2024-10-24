73856c9f9c3f7a9edfc03c26716b7826e39eeba3
mpesaran <mahsa.pesaran@gmail.com>
2024-10-24 14:58:14 +1100

    Add a function to print numbers until 98

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints number until reaches 98
+ * @n: given number to start from
+ *
+ * Return: void
+*/
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		while (n <= 98)
+		{
+			if ( n == 98)
+			{	
+				printf("%d", n);
+			}		
+			printf("%d\, \n", n);
+			n++;
+		}
+	}
+	else
+	{
+		while (n => 98)
+		{
+			if (n == 98)
+			{
+				printf("%d", n);
+			}
+			printf("%d\, \n", n);
+			n--;
+		}
+	}
+}
