efff424fa7f41521aa942cd3a3e028bb930e7afc
mpesaran <mahsa.pesaran@gmail.com>
2024-10-24 15:27:48 +1100

    Add an else statment to handle when n is not 98

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -9,20 +9,18 @@
 */
 void print_to_98(int n)
 {
-	if (n == 98)
-	{
-		printf("98\n");
-	}
-	else if (n < 98)
+	if (n <= 98)
 	{
 		while (n <= 98)
 		{
 			if ( n == 98)
 			{
 				printf("%d\n", n);
-				n++;
 			}
-			printf("%d, ", n);
+			else
+			{
+				printf("%d, ", n);
+			}
 			n++;
 		}
 	}
@@ -33,9 +31,11 @@ void print_to_98(int n)
 			if (n == 98)
 			{
 				printf("%d\n", n);
-				n--;
 			}
-			printf("%d, ", n);
+			else
+			{
+				printf("%d, ", n);
+			}
 			n--;
 		}
 	}
