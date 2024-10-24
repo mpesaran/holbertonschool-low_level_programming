50b8d316595a9c8344f517bf6abf389499ae2444
mpesaran <mahsa.pesaran@gmail.com>
2024-10-24 15:03:33 +1100

    Edit errors for printing format

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -17,19 +17,19 @@ void print_to_98(int n)
 			{	
 				printf("%d", n);
 			}		
-			printf("%d\, \n", n);
+			printf("%d, \n", n);
 			n++;
 		}
 	}
 	else
 	{
-		while (n => 98)
+		while (n >= 98)
 		{
 			if (n == 98)
 			{
 				printf("%d", n);
 			}
-			printf("%d\, \n", n);
+			printf("%d, \n", n);
 			n--;
 		}
 	}
