8f7b06c9128cebd92474e0e5fb6c3baff5957bfd
mpesaran <mahsa.pesaran@gmail.com>
2024-10-28 09:49:05 +1100

    Change the if conditions

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -6,9 +6,13 @@
 void print_most_numbers(void)
 {
 	int i = 48;
-	while ( i <= 57)
+	while (i <= 57)
 	{
-		if (i != 50 || i != 52)
+		if (i == 50 || i == 52)
+		{
+			break;
+		}
+		else
 		{
 			_putchar(i);
 		}
