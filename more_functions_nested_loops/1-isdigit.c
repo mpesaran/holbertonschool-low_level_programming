bef55eb0dce0714d8d12f4025ca091761252e2f4
mpesaran <mahsa.pesaran@gmail.com>
2024-10-28 08:31:04 +1100

    Change the condition of if statement

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
-/** _isdigit - check if the character is a singlr digit
+/**
+ * _isdigit - check if the character is a singlr digit
  * @c: the character to be checked
- *
  *
  * Return: 1 if is a single digit, 0 otherwise
  */
 int _isdigit(int c)
 {
-	if (c <= 48 && c >= 57)
+	if (c >= 48 && c <= 57)
 	{
 		return (1);
 	}
