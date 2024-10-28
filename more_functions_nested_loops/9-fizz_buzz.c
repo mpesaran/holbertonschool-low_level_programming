802a1ccb1c31a14732b596839fde2460da398cc3
mpesaran <mahsa.pesaran@gmail.com>
2024-10-28 14:04:44 +1100

    Print 1 to 100 except multiplies of 3 and 5

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * msin - prints 1 to 100 except multiplies of 3, 5 and 15
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	int n = 1;
+
+	while (n <= 100)
+	{
+		if (n % 3 == 0 && n % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (n % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (n % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", n);
+		}
+		n++;
+	}
+	printf("\n");
+	return (0);
+}
