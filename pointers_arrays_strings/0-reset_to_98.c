19b74bbbfa1ccd1df061a20e0d8a6fc50529d607
mpesaran <mahsa.pesaran@gmail.com>
2024-10-29 17:05:44 +1100

    Add a functin to reset the pointer value

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - update the value of a pointer to 98
+ *@*n: the pointer to change the value of
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This is the second project of the week 4. It's about pointers and data structures. Let's practice!
