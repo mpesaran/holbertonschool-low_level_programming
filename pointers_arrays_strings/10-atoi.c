ac513985befa3eeb96a82cd4db252008c27dfddf
mpesaran <mahsa.pesaran@gmail.com>
2024-10-30 15:40:52 +1100

    convert a string to int

---
+++ pointers_arrays_strings/10-atoi.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * 
+ */
+int _atoi(char *s)
+{
+	int result;
+	int sign = 1;
+	
+	while (*s)
+	{
+		if (*s < '0' || (*s > '9' && *s != '-' && *s != '+'))
+		{
+			s++;
+			continue;
+		}
+		if (*s == '-')
+		{
+			sign *= -1;
+			s++;
+		}
+		else if (*s >= '0' && *s =< '9')
+		{
+			result = result * 10 + (*s - '0');
+			s++;
+		}
+		else
+		{
+			break;
+		}
+	}
+	return (result * sign);
+}
