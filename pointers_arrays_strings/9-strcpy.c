9f18d8ff72b4f5573d77180e4970d29d4c52c456
mpesaran <mahsa.pesaran@gmail.com>
2024-10-30 13:49:12 +1100

    Copy a string and return the destination

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * *_strcpy - copies the string from src to dest
+ * @dest: pointer to the destination
+ * @src: pointer to the sourse string
+ *
+ * Return: starting address of destination
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char *start = dest;
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (*start);
+}
