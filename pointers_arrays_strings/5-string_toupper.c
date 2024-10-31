61451d43ca0f751fe304c38cd03df9b7b8adfcf7
mpesaran <mahsa.pesaran@gmail.com>
2024-11-01 08:45:13 +1100

    Add documnetation and return value

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -1,17 +1,22 @@
 #include "main.h"
 
 /**
+ * *string_toupper - changes all lowercase to uppercase
+ * @s: string to be processed
  *
+ * Return: Modified string
  */
 char *string_toupper(char *s)
 {
+	*ptr = s;
+
 	while (*s != '\0')
 	{
-		if (*s >= 97 && *s <= 122)
+		if (*s >= 'a' && *s <= 'z')
 		{
 			*s = *s - 32;
 		}
 		s++;
 	}
-	return (*s);
+	return (ptr);
 }
