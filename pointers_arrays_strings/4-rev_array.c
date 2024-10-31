79f6e74b7a37cba80735678c75e425de2a6f5aa4
mpesaran <mahsa.pesaran@gmail.com>
2024-11-01 08:14:08 +1100

    Add a function to reverse an array

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *reverse_array - reverses the content of an array
+ *@a: the array to be reversed
+ *@n: nuber of elements in array
+ */
+void reverse_array(int *a, int n)
+{
+	int *ptr;
+
+	while (*a != '\0')
+	{
+		*ptr = *a[n-1];
+		ptr++;
+		a++;
+	}
+}
