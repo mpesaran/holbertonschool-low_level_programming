5eef02ccaffd2d45f13987522d7ad55707874e05
mpesaran <mahsa.pesaran@gmail.com>
2024-11-01 07:10:31 +1100

    Edit condition and data type

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -7,9 +7,9 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	char *ptr = dest;
 
-	while (*src ='\0' && n > 0)
+	while (*src != '\0' && n > 0)
 	{
-		ptr = *src;
+		*ptr = *src;
 		ptr++;
 		src++;
 		n--;
