1b55cc5efa4fefe4b2a5ed35654274b75dac4732
mpesaran <mahsa.pesaran@gmail.com>
2024-11-01 07:54:16 +1100

    Add areturn value to stop function

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -11,11 +11,11 @@ int _strcmp(char *s1, char *s2)
 {
 	while (*s1 != '\0' && *s2 != '\0')
 	{
-		if (s1 > s2)
+		if (*s1 > *s2)
 		{
 			return (15);
 		}
-		else if (s1 < s2)
+		else if (*s1 < *s2)
 		{
 			return (-15);
 		}
@@ -25,5 +25,7 @@ int _strcmp(char *s1, char *s2)
 		}
 		s1++;
 		s2++;
+
+		return (0);
 }
 
