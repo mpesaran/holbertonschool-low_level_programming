407b1448bb2d7656f5cada60037638033fdc4b6b
mpesaran <mahsa.pesaran@gmail.com>
2024-11-01 07:33:17 +1100

    Add documenting and a new while loop

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,7 +1,12 @@
 #include "main.h"
 
 /**
+ * *_strncpy - Copies a string to another until reaches n 
+ * @src: the string to be copied to
+ * @dest: the string to be copied on
+ * @n: number of bytes to be copied
  *
+ * Return: resulting string
  */
 char *_strncpy(char *dest, char *src, int n)
 {
@@ -12,6 +17,12 @@ char *_strncpy(char *dest, char *src, int n)
 		*ptr = *src;
 		ptr++;
 		src++;
+		n--;
+	}
+	while (n > 0)
+	{
+		*ptr = '\0';
+		ptr++,
 		n--;
 	}
 	return (dest);
