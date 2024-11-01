14b64f89e3a94b222803a250c300e6b93a1ba495
mpesaran <mahsa.pesaran@gmail.com>
2024-11-01 12:15:00 +1100

    Edit argument names

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -14,11 +14,11 @@ char *leet(char *s)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; letter[j] != '\0'; j++)
+		for (j = 0; letters[j] != '\0'; j++)
 		{
-			if (s[i] == lettes[j])
+			if (s[i] == letters[j])
 			{
-				s[i] = replacemnets[j];
+				s[i] = replacements[j];
 				break;
 			}
 		}
