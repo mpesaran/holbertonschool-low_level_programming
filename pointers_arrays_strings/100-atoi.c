6a6913b862e00b529c6052768b7cdbf972e9759d
mpesaran <mahsa.pesaran@gmail.com>
2024-11-01 15:41:04 +1100

    Handling when string is min int

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,11 +1,12 @@
 #include "main.h"
+#include <limits.h>
 
 /**
  * 
  */
 int _atoi(char *s)
 {
-	int i= 0;
+	int i = 0;
 	int sign = 1;
 	int result = 0;
 
@@ -23,7 +24,9 @@ int _atoi(char *s)
 		else if (s[i] >= '0' && s[i] <= '9')
 		{	
 			result = result * 10 + (s[i] - '0');
-			if (s[i+1] < '0' || s[i+1] > '9')
+			if (result * sign == INT_MIN)
+			       return (INT_MIN)	
+			if (s[i + 1] < '0' || s[i + 1] > '9')
 			{
 				break;
 			}		
