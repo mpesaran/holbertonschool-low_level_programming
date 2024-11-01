01496614c7ef6c4fdbb1475362c1df7d497472cb
mpesaran <mahsa.pesaran@gmail.com>
2024-11-01 15:59:14 +1100

    Add a codition to check min int

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -9,7 +9,7 @@ int _atoi(char *s)
 	int i = 0;
 	int sign = 1;
 	int result = 0;
-
+	int digit;
 
 	while (s[i] != '\0')
 	{
@@ -23,9 +23,10 @@ int _atoi(char *s)
 		}		
 		else if (s[i] >= '0' && s[i] <= '9')
 		{	
-			result = result * 10 + (s[i] - '0');
-			if (result * sign == INT_MIN)
-			       return (INT_MIN)	
+			digit = (s[i] - '0');
+			if (sign == -1 && result == INT_MIN / 10 && digit == 8)
+			       return (INT_MIN);
+			result = result * 10 + digit;
 			if (s[i + 1] < '0' || s[i + 1] > '9')
 			{
 				break;
