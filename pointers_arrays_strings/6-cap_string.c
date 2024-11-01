7bf7b57f3b845f8ebc75ea8de6821db9a1c9d286
mpesaran <mahsa.pesaran@gmail.com>
2024-11-01 11:53:53 +1100

    Add a function to acpitalize evry words starting letter

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * *cap_string - capitalize stating letter of each word
+ * @c: the string to check
+ *
+ * Return: the formatted string
+ */
+char *cap_string(char *c)
+{
+	int i = 0;
+
+	if (c[i] >= 'a' && c[i] <= 'z')
+	{
+		c[i] -= 32;
+	}
+	while (c[i] != '\0')
+	{
+		if ((c[i - 1] == ' ' || c[i - 1] == ',' || c[i - 1] == '!' ||
+			c[i - 1] == '\n' || c[i - 1] == '\t' ||
+			c[i - 1] == '.') && c[i] >= 'a' && c[i] <= 'z')
+		{
+			c[i] -= 32;
+		}
+		i++;
+	}
+	return (c)
+}
