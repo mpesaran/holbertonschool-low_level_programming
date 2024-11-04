1ae223628b528d770651cdb82cf03da521f2ce7b
mpesaran <mahsa.pesaran@gmail.com>
2024-11-04 13:32:29 +1100

    Add a function fills memory with constant byte

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *_memset - fills n first bytes of string with b
+ *@n: number of bytes
+ *@b: constant byte
+ *@s: the string to be replaced
+ *
+ * Return: a pointer to the meory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	while (n > 0)
+	{
+		*s = b;
+		n--;
+		s++;
+	}
+	return (*s)
+}
