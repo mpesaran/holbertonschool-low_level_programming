f4782762309f68ceadb591fc3d5920b3f07379fb
mpesaran <mahsa.pesaran@gmail.com>
2024-11-08 13:36:39 +1100

    Add a function to multiply 2 integers enterred

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * main - multiplies second and third argumants
+ * @argc: number of arguments
+ * @argv: array of strings
+ *
+ * Return: 0 if successful, 1 if just there is one integer
+ */
+int main(int argc, char *argv[])
+{
+	int result, num1, num2;
+
+	if (argc == 2)
+		return (1);
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+
+	result = num1 * num2;
+
+	print_number(result);
+	_putchar('\n');
+	return (0);
+}
+/**
+ * print_number - prints numbers using _putchar
+ * @n: number to print on screen
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n / 10)
+	{
+		print_number(n / 10);
+	}
+	_putchar((n % 10) + '0');
+}
