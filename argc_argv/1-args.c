f05f5c7e68a556cf4258394a6da39800f29721de
mpesaran <mahsa.pesaran@gmail.com>
2024-11-08 12:08:00 +1100

    Add styles

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,13 +1,18 @@
 #include "main.h"
 
 /**
+ * main - prints number of argumnets
+ * @argc: number of arguments
+ * @argv: string of arrays
  *
+ * Return: 0 if successful
  */
 int main(int argc, char *argv[])
 {
 	if (*argv)
 	{
 		_putchar('0' + argc);
+		_putchar('\n');
 	}
 	return (0);
 }
