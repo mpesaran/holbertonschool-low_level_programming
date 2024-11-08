4466883a105cf299bc88d305dee7fe2804908f68
mpesaran <mahsa.pesaran@gmail.com>
2024-11-08 13:40:09 +1100

    Add an Error to special conditions output

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -12,7 +12,15 @@ int main(int argc, char *argv[])
 	int result, num1, num2;
 
 	if (argc == 2)
+	{
+		_putchar('E');
+		_putchar('r');
+		_putchar('r');
+		_putchar('o');
+		_putchar('r');
+		_putchar('\n');
 		return (1);
+	}
 
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[2]);
