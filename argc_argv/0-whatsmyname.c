26ba63f1d0fe39527db33d48940e57ffee7f6eea
mpesaran <mahsa.pesaran@gmail.com>
2024-11-08 11:40:22 +1100

    Change the styles

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,14 +1,18 @@
 #include "main.h"
 /**
+ * main - prints function name
+ * @argc: number of arguments
+ * @argv: array of string
  *
- *
+ *Return - 0 if it was successful
  */
 int main(int argc, char *argv[])
 {
 	int i = 0;
+
 	if (argc > 0)
 	{
-		while(argv[0][i])
+		while (argv[0][i])
 		{
 			_putchar(argv[0][i]);
 			i++;
