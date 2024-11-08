709e23df193013f1a81a988d70354c209f41b54c
mpesaran <mahsa.pesaran@gmail.com>
2024-11-08 11:31:02 +1100

    Add a function to prints the function name

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *
+ *
+ */
+int main(int argc, char *argv[])
+{
+	int i = 0;
+	if (argc > 0)
+	{
+		while(argv[0][i])
+		{
+			_putchar(argv[0][i]);
+			i++;
+		}
+	}
+	_putchar('\n');
+	return (0);
+}
