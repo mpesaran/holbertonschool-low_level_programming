a61d28025cfdb11854f9dd90265df32959e8e514
mpesaran <mahsa.pesaran@gmail.com>
2024-11-11 12:38:17 +1100

    Delete strlen function and calculate len

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,22 +1,28 @@
 #include "main.h"
 #include <stdlib.h>
-#include <string.h>
+
 /**
  *
  */
 char *_strdup(char *str)
 {
 	char *dup;
+	char *original = str;
 	int len, i;
 
 	if (str == NULL)
 	{
 		return (NULL);
 	}
-	len = strlen(str);
+	while (*str != '\0')
+	{
+		len++;
+		str++;
+	}
 	dup = malloc((len + 1) * sizeof(char));
 	if (dup == NULL)
 		return (NULL);
+	str = original;
 	for (i = 0; i < len; i++)
 		dup[i] = str[i];
 	dup[len] = '\0';
