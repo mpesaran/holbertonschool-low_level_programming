203375550bc1a123757f3872023227014c64256d
mpesaran <mahsa.pesaran@gmail.com>
2024-11-11 16:05:43 +1100

    Add a function to concate args in a string

---
+++ malloc_free/100-argstostr.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *argstostr - takes argumnets and concates tham in a string
+ * @ac: number of arguments
+ * @av: array of args
+ *
+ * Return: concatinated string
+ */
+char *argstostr(int ac, char **av)
+{
+	char *str;
+	int i, j, k = 0;
+	int total_length = 0;
+	
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			total_length++;
+		}
+		total_length++;
+	}
+	str = malloc((total_length + 1) * sizeof(char));
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			str[k++] = av[i][j];
+		}
+		str[k++] = '\n';
+	}
+	str[k] = '\0';
+	return (str);
+}
