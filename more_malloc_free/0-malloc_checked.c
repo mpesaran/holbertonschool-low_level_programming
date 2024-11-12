8cbc1747e9029c8405f03e407f9d9a44f7ba379e
mpesaran <mahsa.pesaran@gmail.com>
2024-11-12 16:26:51 +1100

    Add detailes to description

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -3,6 +3,8 @@
 /**
  * *malloc_checked - allocates memory using malloc
  *@b: number of elements need meory
+ *
+ * Return: 98 if not allocated, otherwise 0
  */
 void *malloc_checked(unsigned int b)
 {
