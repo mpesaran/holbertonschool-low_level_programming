59f99ebfef178af1e40a653986983c4cc9876c88
mpesaran <mahsa.pesaran@gmail.com>
2024-11-12 16:08:33 +1100

    Add comments for the function

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,7 +1,8 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- *
+ * *malloc_checked - allocates memory using malloc
+ *@b: number of elements need meory
  */
 void *malloc_checked(unsigned int b)
 {
