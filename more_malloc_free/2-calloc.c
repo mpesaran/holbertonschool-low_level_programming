512e5cbd3d3f350b1b3b7afe80a84f479332d315
mpesaran <mahsa.pesaran@gmail.com>
2024-11-13 09:32:10 +1100

    Set values to zero

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -11,8 +11,10 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	arr = malloc((nmemb + 1) * size);
-	for (i = 0; i <= nmemb; i++)
+	arr = malloc(nmemb * size);
+	if (arr == NULL)
+		return (NULL);
+	for (i = 0; i < nmemb * size; i++)
 	{
 		arr[i] = 0;
 	}
