0f45837af50c61d28cba14e7b18b50d56285d550
mpesaran <mahsa.pesaran@gmail.com>
2024-11-13 08:50:58 +1100

    Add a function to allocate specific amount of memory

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ *
+ *
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	char *arr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	arr = malloc((nmemb + 1) * size);
+	for (i = 0; i <= nmemb; i++)
+	{
+		arr[i] = '0';
+	}
+	return (arr);
+}
