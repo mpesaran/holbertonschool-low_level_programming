b95d18b06c92867f6b6be961688aedd0aa708bcd
mpesaran <mahsa.pesaran@gmail.com>
2024-11-14 09:45:28 +1100

    Handle null conditions

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -6,6 +6,10 @@
  */
 void print_dog(struct dog *d)
 {
+	if ((*d).name == NULL)
+		printf("Name: %s\n", "(nil)");
+	if((*d).owner == NULL)
+		printf("Owner: %s\n", "(nill)");
 	printf("Name: %s\n", (*d).name);
 	printf("Age: %f\n", (*d).age);
 	printf("Owner: %s\n", (*d).owner);
