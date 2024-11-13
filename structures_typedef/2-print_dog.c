cbaf52be8500987b45354f376cc99c9354f8166f
mpesaran <mahsa.pesaran@gmail.com>
2024-11-14 09:54:52 +1100

    Change the if statements

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -7,10 +7,20 @@
 void print_dog(struct dog *d)
 {
 	if ((*d).name == NULL)
+	{
 		printf("Name: %s\n", "(nil)");
-	if((*d).owner == NULL)
-		printf("Owner: %s\n", "(nill)");
-	printf("Name: %s\n", (*d).name);
-	printf("Age: %f\n", (*d).age);
-	printf("Owner: %s\n", (*d).owner);
+	}
+	else
+	{
+		printf("Name: %s\n", d->name);
+	}
+	printf("Age: %f\n", d->age);
+	if ((*d).owner == NULL)
+	{
+		printf("Owner: %s\n", "(nil)");
+	}
+	else
+	{
+		printf("Owner: %s\n", (*d).owner);
+	}
 }
