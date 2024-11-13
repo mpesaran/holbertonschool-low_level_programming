5698e5b08ade3ef10d7448d4e7bca85eb000fffe
mpesaran <mahsa.pesaran@gmail.com>
2024-11-13 16:23:30 +1100

    Add a structure data type

---
+++ structures_typedef/README.md
@@ -0,0 +1,1 @@
+These tasks are about structure data types and typedef.
---
+++ structures_typedef/dog.h
@@ -0,0 +1,12 @@
+/**
+ * struct dog - creates a dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float *age;
+	char *owner;
+}
