dfaa85e3c7849b30ae54bc4fd0059b3ad44458c0
mpesaran <mahsa.pesaran@gmail.com>
2024-11-15 07:16:32 +1100

    Add a finction to create a new dog type

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -8,25 +8,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *ndog;
 
-	ndog = malloc(sizeof(dog_t));
-	if (ndog == NULL)
-	{
-		return NULL;
-	}
-	if (name != NULL)
-	{
-		ndog->name = malloc(strlen(name) + 1);
-		if (ndog->name == NULL)
-		{
-			free(ndog);
-			return (NULL);
-		}
-		strcpy(ndog->name, name);
-	}
-	else
-	{
-		ndog->name = NULL;
-	}
+	ndog->name = name;
 	ndog->age = age;
 	ndog->owner = owner;
 	return (ndog);
