516a9e6996fa6d428c3e87a3d37902a91ee90854
mpesaran <mahsa.pesaran@gmail.com>
2024-11-15 07:37:27 +1100

    Add copying the name and owner variabls

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,18 +1,42 @@
 #include "dog.h"
 #include <stdlib.h>
-#include <string.h>
 /**
+ * *new_dog - creates a new dog type dog_t
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner's name
  *
+ * Return: always -
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *ndog;
-	
+	int name_len = 0, owner_len = 0, i;
+
+	while (name[name_len] != '\0')
+		name_len++;
+	while (owner[owner_len] != '\0')
+		owner_len++;
 	ndog = malloc(sizeof(dog_t));
 	if (ndog == NULL)
 		return (NULL);
-	ndog->name = name;
+	ndog->name = malloc((name_len + 1) * sizeof(char));
+	if (ndog->name == NULL)
+	{
+		free(ndog);
+		return (NULL);
+	}
+	ndog->owner = malloc((owner_len + 1) * sizeof(char));
+	if (ndog->owner == NULL)
+	{
+		free(ndog->name);
+		free(ndog);
+		return (NULL);
+	}
+	for (i = 0; i < name_len; i++)
+		ndog->name[i] = name[i];
+	for (i = 0; i < owner_len; i++)
+		ndog->owner[i] = owner[i];
 	ndog->age = age;
-	ndog->owner = owner;
 	return (ndog);
 }
