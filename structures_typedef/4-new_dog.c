fabbb84def8dc58c2b9e74aef6b0e53296449258
mpesaran <mahsa.pesaran@gmail.com>
2024-11-15 07:18:27 +1100

    Initialize the new memory space

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -7,7 +7,10 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *ndog;
-
+	
+	ndog = malloc(sizeof(dog_t));
+	if (ndog == NULL)
+		return (NULL);
 	ndog->name = name;
 	ndog->age = age;
 	ndog->owner = owner;
