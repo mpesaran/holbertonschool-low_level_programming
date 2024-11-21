a25e5c70afc18fd31b83e60f696ba51e8219f2fb
mpesaran <mahsa.pesaran@gmail.com>
2024-11-21 12:21:09 +1100

    Add a function to print based on the format

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,69 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ *
+ */
+typedef struct format_spec
+{
+	char t;
+	void (*f)(va_list args);
+}fmt_t;
+void print_char(va_list args)
+{
+	printf("%c", va_arg(args, int));
+}
+void print_int(va_list args)
+{
+	printf("%i", va_arg(args, int));
+}
+void print_float(va_list args)
+{
+	printf("%f", va_arg(args, double));
+}
+void print_string(va_list args)
+{
+	char *str = va_arg(args, char *);
+	if (*str == '\0')
+	{
+		str = "(nill)";
+	}
+	printf("%s", str);
+}
+/**
+ *
+ */
+void print_all(const char * const format, ...)
+{
+	int i = 0, j;
+	va_list args;
+
+	fmt_t formats[] = {
+        	{'c', print_char},
+        	{'i', print_int},
+       		{'f', print_float},
+        	{'s', print_string},
+        	{'\0', NULL}
+        };
+
+	va_start(args, format);
+	while (format[i] != '\0')
+	{
+		j = 0;
+		while (formats[j].t != '\0')
+		{
+			if (format[i] == formats[j].t)
+			{
+				formats[j].f(args);
+				if (format[i + 1] != '\0')
+				{
+					printf(", ");
+				}
+			}
+			j++;
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(args);
+}
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,7 +1,13 @@
+#include <stdarg.h>
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+void print_char(va_list args);
+void print_int(va_list args);
+void print_float(va_list args);
+void print_string(va_list args);
 
 #endif
