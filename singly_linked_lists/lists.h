caf7ed1d9c1ec6c838c188f9c7fd55cdb967dc10
mpesaran <mahsa.pesaran@gmail.com>
2024-12-02 13:43:54 +1100

    Change the prototype in header file

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,5 +1,6 @@
 #ifndef LISTS_H
 #define LISTS_H
+#include <stddef.h>
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -16,5 +17,6 @@ typedef struct list_s
 } list_t;
 
 int _putchar(char c);
+size_t print_list(const list_t *h);
 
 #endif
