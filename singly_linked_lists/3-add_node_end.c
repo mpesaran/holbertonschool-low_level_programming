8e3282e03b62c816a142296fda74cf002822a245
mpesaran <mahsa.pesaran@gmail.com>
2024-12-02 17:04:48 +1100

    Add a null case for the list

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -37,6 +37,11 @@ list_t *add_node_end(list_t **head, const char *str)
 	}
 	new_node->len = _strlen(str);
 	new_node->next = NULL;
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
 	current = *head;
 	while (current->next != NULL)
 		current = current->next;
