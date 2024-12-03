414351e57c8fe99c26013169d6666bae095478a1
mpesaran <mahsa.pesaran@gmail.com>
2024-12-03 16:08:51 +1100

    Add a function to print alis elements

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stddef.h>
+#include <stdio.h>
+/**
+ * print_dlistint - prints all the elements of a dlistint_t
+ * @h: pointer to the beginning of a list
+ *
+ * Return: length of the list
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	const dlistint_t *curr = h;
+	size_t len = 0;
+	int t;
+
+	while (curr != NULL)
+	{
+		t = curr->n;
+		_putchar(t + '0');
+		-putchar('\n');
+		len++;
+		curr = curr->next;
+	}
+	return (len);
+}
---
+++ doubly_linked_lists/README.md
@@ -0,0 +1,1 @@
+We will practice doubly linked lists here.
