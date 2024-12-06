9d132ec3e31d8a3d6c1516d6ff65d64bc5d5a413
mpesaran <mahsa.pesaran@gmail.com>
2024-12-06 13:49:06 +1100

    Check more condition to add new node

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -11,36 +11,43 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	unsigned int len = 0;
-	dlistint_t *curr = *h;
-	dlistint_t *new_node;
+	dlistint_t *curr = *h, *new_node, *tail = *h;
 
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
-	if (idx == 1)
+	if (idx == 0)
 	{
 		new_node->next = *h;
+		new_node->prev = NULL;
 		if (h != NULL)
 			(*h)->prev = new_node;
 		*h = new_node;
 		return (new_node);
 	}
-	while (curr != NULL)
+	while (curr != NULL && len < idx)
 	{
-		if (idx == len)
-		{
-			new_node->prev = curr->prev;
-			new_node->next = curr;
-			curr->prev = new_node;
-			if (new_node->next != NULL)
-				new_node->next->prev = new_node;
-			return (new_node);
-		}
-		len++;
 		curr = curr->next;
+		len++;
 	}
-	if (len < idx)
+	if (len != idx)
+	{
 		return (NULL);
+	}
+	if (curr == NULL)
+	{
+		while (tail->next != NULL)
+			tail = tail->next;
+		new_node->next = NULL;
+		new_node->prev = tail;
+		tail->next = new_node;
+		return (new_node);
+	}
+	new_node->next = curr;
+	new_node->prev = curr->prev;
+	if (curr->prev != NULL)
+		curr->prev->next = new_node;
+	curr->prev = new_node;
 	return (new_node);
 }
