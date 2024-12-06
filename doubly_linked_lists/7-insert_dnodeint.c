a5ad4045209c8b8b5fe2843d882295618cfc2f97
mpesaran <mahsa.pesaran@gmail.com>
2024-12-06 14:22:35 +1100

    Chenge the sequence of code

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -13,12 +13,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int i = 0;
 	dlistint_t *curr = *h, *new_node;
 
-	new_node = malloc(sizeof(dlistint_t));
-	if (new_node == NULL)
-		return (NULL);
-	new_node->n = n;
 	if (idx == 0)
-		(add_dnodeint(h, n));
+		return (add_dnodeint(h, n));
 	while (curr != NULL && i < idx - 1)
 	{
 		curr = curr->next;
@@ -26,11 +22,14 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 	if (curr == NULL)
 	{
-		free(new_node);
 		return (NULL);
 	}
-	if (curr->next == NULL)
+	if (curr->next == NULL && i ==idx - 1)
 		return (add_dnodeint_end(h, n));
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
 	new_node->next = curr->next;
 	new_node->prev = curr;
 	if (curr->next != NULL)
