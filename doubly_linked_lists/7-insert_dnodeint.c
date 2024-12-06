65b7375281480c67c7be298295e38fe5306e020c
mpesaran <mahsa.pesaran@gmail.com>
2024-12-06 13:58:59 +1100

    Dereference the h pointer

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -21,7 +21,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		new_node->next = *h;
 		new_node->prev = NULL;
-		if (h != NULL)
+		if (*h != NULL)
 			(*h)->prev = new_node;
 		*h = new_node;
 		return (new_node);
